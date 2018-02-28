532ccf5753a982fb66592bd522f90421bb1a1b7b
adamjirik <adam.jirik1@gmail.com>
2018-02-28 22:43:29 +0100

    initializing project

---
+++ src/test/java/com/englishsolutions/adamjirik/languageschool/LanguageSchoolApplicationTests.java
@@ -0,0 +1,16 @@
+package com.englishsolutions.adamjirik.languageschool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LanguageSchoolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
